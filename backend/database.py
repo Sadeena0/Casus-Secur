5998af2949d575a4eaa29c5f64122a1a63c8cf8a
creeperQ7 <42542637+creeperQ7@users.noreply.github.com>
2025-04-04 15:24:52 +0200

    Implement network monitoring and database registration

--- backend/database.py
+++ backend/database.py
@@ -20,4 +20,22 @@ def intialise_database():
     connection = sqlite3.connect('connections.db')
     with connection as conn:
         cur = conn.cursor()
-        cur.execute("CREATE TABLE ip_addresses(ip PRIMARY KEY, location, times int CHECK(times>0))")
+        cur.execute("CREATE TABLE ip_addresses(ip PRIMARY KEY, pid int, appname varchar(255),"
+                    " location varchar(255), times int CHECK(times>0))")
+
+def add_records(records):
+    connection = sqlite3.connect('connections.db')
+    with connection as conn:
+        cur = conn.cursor()
+        for record in records:
+            res = cur.execute("SELECT ip FROM ip_addresses WHERE ip=?", (f"{record[0]}:{record[1]}",))
+            if res.fetchone():
+                cur.execute("SELECT times from ip_addresses WHERE ip=?", (f"{record[0]}:{record[1]}",))
+                times = cur.fetchone()[0] + 1
+                cur.execute("UPDATE ip_addresses SET times=? WHERE ip=?", (times, f"{record[0]}:{record[1]}",))
+                pass
+            else:
+                cur.execute("INSERT INTO ip_addresses(ip,pid,appname,times) VALUES(?,?,?,?) "
+                            , (f"{record[0]}:{record[1]}", record[2], record[3], 1))
+
+
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from database import *
+import threading
+import psutil
 
 match open_connection():
     case 0:
@@ -9,3 +11,26 @@ match open_connection():
     case 2:
         print("Empty database detected. Initializing database.")
         intialise_database()
+
+def network_scan():
+    threading.Timer(1, network_scan).start()
+    connections = psutil.net_connections(kind='inet')
+
+    log_entries = []
+
+    for conn in connections:
+        if conn.status == 'ESTABLISHED' and conn.raddr:
+            pid = conn.pid
+            if pid:
+                try:
+                    process = psutil.Process(pid)
+                    app_name = process.name()
+                except psutil.NoSuchProcess:
+                    app_name = "Unknown"
+            else:
+                app_name = "System"
+
+            log_entries.append((conn.raddr[0], conn.raddr[1], pid, app_name, "Not Implemented"))
+    add_records(log_entries)
+
+network_scan()
