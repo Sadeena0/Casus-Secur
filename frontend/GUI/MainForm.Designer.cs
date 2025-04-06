5e30cd0769bb6fa0cb927513cf1cf959cbeadc4a
Britt <brittmbosch@gmail.com>
2025-04-06 17:39:52 +0200

    added database to frontend
    to do: fix connection string, scaling

--- frontend/GUI/MainForm.Designer.cs
+++ frontend/GUI/MainForm.Designer.cs
@@ -28,11 +28,14 @@ namespace GUI {
         private void InitializeComponent() {
             this.MainSplitContainer = new System.Windows.Forms.SplitContainer();
             this.MapButtonsSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).BeginInit();
             this.MainSplitContainer.Panel1.SuspendLayout();
+            this.MainSplitContainer.Panel2.SuspendLayout();
             this.MainSplitContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.MapButtonsSplitContainer)).BeginInit();
             this.MapButtonsSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // MainSplitContainer
@@ -44,6 +47,10 @@ namespace GUI {
             // MainSplitContainer.Panel1
             // 
             this.MainSplitContainer.Panel1.Controls.Add(this.MapButtonsSplitContainer);
+            // 
+            // MainSplitContainer.Panel2
+            // 
+            this.MainSplitContainer.Panel2.Controls.Add(this.dataGridView1);
             this.MainSplitContainer.Size = new System.Drawing.Size(1258, 664);
             this.MainSplitContainer.SplitterDistance = 800;
             this.MainSplitContainer.TabIndex = 0;
@@ -53,10 +60,21 @@ namespace GUI {
             this.MapButtonsSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MapButtonsSplitContainer.Location = new System.Drawing.Point(0, 0);
             this.MapButtonsSplitContainer.Name = "MapButtonsSplitContainer";
-            this.MapButtonsSplitContainer.Size = new System.Drawing.Size(419, 664);
-            this.MapButtonsSplitContainer.SplitterDistance = 800;
-            this.MapButtonsSplitContainer.TabIndex = 0;
             this.MapButtonsSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.MapButtonsSplitContainer.Size = new System.Drawing.Size(800, 664);
+            this.MapButtonsSplitContainer.SplitterDistance = 390;
+            this.MapButtonsSplitContainer.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(454, 664);
+            this.dataGridView1.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -66,10 +84,12 @@ namespace GUI {
             this.Text = "IP Tracker";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.MainSplitContainer.Panel1.ResumeLayout(false);
+            this.MainSplitContainer.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).EndInit();
             this.MainSplitContainer.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.MapButtonsSplitContainer)).EndInit();
             this.MapButtonsSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -78,6 +98,7 @@ namespace GUI {
 
         private System.Windows.Forms.SplitContainer MainSplitContainer;
         private System.Windows.Forms.SplitContainer MapButtonsSplitContainer;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- frontend/GUI/MainForm.cs
+++ frontend/GUI/MainForm.cs
@@ -9,15 +9,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
 
 namespace GUI {
-    public partial class MainForm : Form {
-        public MainForm() {
+    public partial class MainForm : Form 
+    {
+        private string connectionString = @"Data Source=C:\Users\britt\Desktop\school\HBO-ICT jaar 1\Blok 3\Security\casus\Casus-Secur\backend\connections.db";
+        public MainForm() 
+        {
             InitializeComponent();
 
         }
 
-        private void MainForm_Load(object sender, EventArgs e) {
+        private void MainForm_Load(object sender, EventArgs e) 
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT ip, appname, times FROM ip_addresses";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
